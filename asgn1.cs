52ace38a57478441813482b4eefa8e58b6a9a40e
schmibbs <32438797+schmibbs@users.noreply.github.com>
2018-08-28 01:26:39 -0400

    Assignment1 1.3
    Got rid of the border since I realized that they are unnecessary after filling with a color. I have changed the color to super red and super blue. Will look into making the color less intense and adding labels

--- asgn1.cs
+++ asgn1.cs
@@ -15,9 +15,16 @@ class app {
 	Form f = new AssignOne("First Form!", "Joseph");
 	f.Size = new System.Drawing.Size(1167, 500); 	//Source: https://docs.microsoft.com/en-us/dotnet/framework/winforms/how-to-resize-windows-forms
 	Panel p1 = new Panel();
-	p1.BorderStyle = BorderStyle.FixedSingle;	//Source: https://stackoverflow.com/questions/28255438/programmatically-create-panel-and-add-picture-boxes
-	p1.Location = new Point(50, 50);		//Source: https://stackoverflow.com/questions/23648371/point-class-in-c-sharp
+	Panel p2 = new Panel();
+	
+	p1.Size = new System.Drawing.Size(583, 400);
+	p2.Size = new System.Drawing.Size(583, 400);
+	p1.BackColor = Color.Red;			//Source: https://social.msdn.microsoft.com/Forums/en-US/81ee8efd-29cf-40a1-b0e7-5db96c34c970/wanting-to-change-a-forms-background-colour-with-the-click-of-a-radio-button?forum=csharpgeneral
+	p2.BackColor = Color.Blue;
+	p1.Location = new Point(0, 0);			//Source: https://stackoverflow.com/questions/23648371/point-class-in-c-sharp
+	p2.Location = new Point(584, 0);
 	f.Controls.Add(p1);				//Source: https://stackoverflow.com/questions/9401337/adding-panels-to-a-form-in-c-sharp-during-execution
+	f.Controls.Add(p2);
 	Application.Run(f);
 	}
 }
