2e292c4494fb235f036d2b8e84ed93cb7af438a8
schmibbs <32438797+schmibbs@users.noreply.github.com>
2018-08-28 01:13:26 -0400

    Assignment1 1.2
    Added a panel but did not notice it. I drew a border around it and moved it to test out two functions. Will add another panel, and change its color

--- asgn1.cs
+++ asgn1.cs
@@ -1,4 +1,5 @@
 using System.Windows.Forms;
+using System.Drawing;
 
 class AssignOne : Form {
 	static string myName;
@@ -12,7 +13,11 @@ class AssignOne : Form {
 class app {
  static void Main() {
 	Form f = new AssignOne("First Form!", "Joseph");
-	f.Size = new System.Drawing.Size(500, 500); //Source: https://docs.microsoft.com/en-us/dotnet/framework/winforms/how-to-resize-windows-forms
+	f.Size = new System.Drawing.Size(1167, 500); 	//Source: https://docs.microsoft.com/en-us/dotnet/framework/winforms/how-to-resize-windows-forms
+	Panel p1 = new Panel();
+	p1.BorderStyle = BorderStyle.FixedSingle;	//Source: https://stackoverflow.com/questions/28255438/programmatically-create-panel-and-add-picture-boxes
+	p1.Location = new Point(50, 50);		//Source: https://stackoverflow.com/questions/23648371/point-class-in-c-sharp
+	f.Controls.Add(p1);				//Source: https://stackoverflow.com/questions/9401337/adding-panels-to-a-form-in-c-sharp-during-execution
 	Application.Run(f);
 	}
 }
