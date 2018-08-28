e07448c43442bfedcbfd7706fc2519a71db3b99c
schmibbs <32438797+schmibbs@users.noreply.github.com>
2018-08-28 00:16:48 -0400

    Assignment1 1.1
    Added a line to change the size. Will add the panels

--- asgn1.cs
+++ asgn1.cs
@@ -12,6 +12,7 @@ class AssignOne : Form {
 class app {
  static void Main() {
 	Form f = new AssignOne("First Form!", "Joseph");
+	f.Size = new System.Drawing.Size(500, 500); //Source: https://docs.microsoft.com/en-us/dotnet/framework/winforms/how-to-resize-windows-forms
 	Application.Run(f);
 	}
 }
