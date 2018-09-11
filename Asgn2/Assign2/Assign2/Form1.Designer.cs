6b47e5a8e87af92e0baca2be64c3c89a187c943f
schmibbs <32438797+schmibbs@users.noreply.github.com>
2018-09-11 01:26:52 -0400

    Assign2
    Set a default size and position in the application settings. Still working on user access via main form

--- Asgn2/Assign2/Assign2/Form1.Designer.cs
+++ Asgn2/Assign2/Assign2/Form1.Designer.cs
@@ -28,10 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            ////this.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::Assign2.Properties.Settings.Default, "Text1", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            ////this.Name = "Form1";
+            ////this.Text = global::Assign2.Properties.Settings.Default.Text1;
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- Asgn2/Assign2/Assign2/Properties/Settings.Designer.cs
+++ Asgn2/Assign2/Assign2/Properties/Settings.Designer.cs
@@ -8,22 +8,48 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Assign2.Properties
-{
-
-
+namespace Assign2.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.7.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute(" 100, 100")]
+        public string UserSettings {
+            get {
+                return ((string)(this["UserSettings"]));
+            }
+            set {
+                this["UserSettings"] = value;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("750, 500")]
+        public global::System.Drawing.Size DefaultSize {
+            get {
+                return ((global::System.Drawing.Size)(this["DefaultSize"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("600, 0")]
+        public global::System.Drawing.Point DefaultLocation {
+            get {
+                return ((global::System.Drawing.Point)(this["DefaultLocation"]));
             }
         }
     }
