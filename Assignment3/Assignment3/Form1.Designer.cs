47020fe37bd818d41d0f77d62820e618f633ea2d
Joseph Adams <jadam090@fiu.edu>
2018-09-17 18:20:08 -0400

    Added a context menu. Does not show up. WIll work on figuring out why

--- Assignment3/Assignment3/Form1.Designer.cs
+++ Assignment3/Assignment3/Form1.Designer.cs
@@ -29,12 +29,51 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.PreferencesMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.openPreferencesModallyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openPreferencesModelesslyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.PreferencesMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // PreferencesMenu
+            // 
+            this.PreferencesMenu.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.PreferencesMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openPreferencesModallyToolStripMenuItem,
+            this.openPreferencesModelesslyToolStripMenuItem});
+            this.PreferencesMenu.Name = "PreferencesMenu";
+            this.PreferencesMenu.Size = new System.Drawing.Size(318, 64);
+            // 
+            // openPreferencesModallyToolStripMenuItem
+            // 
+            this.openPreferencesModallyToolStripMenuItem.Name = "openPreferencesModallyToolStripMenuItem";
+            this.openPreferencesModallyToolStripMenuItem.Size = new System.Drawing.Size(317, 30);
+            this.openPreferencesModallyToolStripMenuItem.Text = "Open Preferences Modally";
+            // 
+            // openPreferencesModelesslyToolStripMenuItem
+            // 
+            this.openPreferencesModelesslyToolStripMenuItem.Name = "openPreferencesModelesslyToolStripMenuItem";
+            this.openPreferencesModelesslyToolStripMenuItem.Size = new System.Drawing.Size(317, 30);
+            this.openPreferencesModelesslyToolStripMenuItem.Text = "Open Preferences Modelessly";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.PreferencesMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ContextMenuStrip PreferencesMenu;
+        private System.Windows.Forms.ToolStripMenuItem openPreferencesModallyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openPreferencesModelesslyToolStripMenuItem;
     }
 }
 
--- Assignment3/Assignment3/Form1.cs
+++ Assignment3/Assignment3/Form1.cs
@@ -16,5 +16,15 @@ namespace Assignment3
         {
             InitializeComponent();
         }
+
+        private void openPreferencesModaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
