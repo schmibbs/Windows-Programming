d9c7253caf0bbd20ff6f211673046925bff0504e
Joseph Adams <jadam090@fiu.edu>
2018-09-26 00:16:12 -0400

    Reorganized Assignments and created a folder for 5

---
+++ Assignment 2/Asgn2/Assign2/Assign2/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace Assign2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Reset = new System.Windows.Forms.Button();
+            this.Location = new System.Windows.Forms.Button();
+            this.Save = new System.Windows.Forms.Button();
+            this.Name = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.Reset);
+            this.groupBox1.Controls.Add(this.Location);
+            this.groupBox1.Controls.Add(this.Save);
+            this.groupBox1.Controls.Add(this.Name);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(586, 125);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "quadButtonTextGroup";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(12, 58);
+            this.textBox1.MaxLength = 15;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(562, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Reset
+            // 
+            this.Reset.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.Reset.Location = new System.Drawing.Point(260, 96);
+            this.Reset.Name = "Reset";
+            this.Reset.Size = new System.Drawing.Size(75, 23);
+            this.Reset.TabIndex = 3;
+            this.Reset.Text = "Reset ";
+            this.Reset.UseVisualStyleBackColor = true;
+            this.Reset.Click += new System.EventHandler(this.Reset_Click);
+            // 
+            // Location
+            // 
+            this.Location.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.Location.Location = new System.Drawing.Point(499, 96);
+            this.Location.Name = "Location";
+            this.Location.Size = new System.Drawing.Size(75, 23);
+            this.Location.TabIndex = 2;
+            this.Location.Text = "Save place";
+            this.Location.UseVisualStyleBackColor = true;
+            this.Location.Click += new System.EventHandler(this.Location_Click);
+            // 
+            // Save
+            // 
+            this.Save.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.Save.Location = new System.Drawing.Point(12, 96);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(75, 23);
+            this.Save.TabIndex = 1;
+            this.Save.Text = "Save size";
+            this.Save.UseVisualStyleBackColor = true;
+            this.Save.Click += new System.EventHandler(this.Save_Click);
+            // 
+            // Name
+            // 
+            this.Name.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.Name.Location = new System.Drawing.Point(255, 19);
+            this.Name.Name = "Name";
+            this.Name.Size = new System.Drawing.Size(75, 23);
+            this.Name.TabIndex = 0;
+            this.Name.Text = "Add Name";
+            this.Name.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.SaddleBrown;
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(0, 125);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(586, 325);
+            this.listBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(586, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.groupBox1);
+            this.HelpButton = true;
+            this.Name = "Form1";
+            this.Text = "MainForm";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button Name;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button Reset;
+        private System.Windows.Forms.Button Location;
+        private System.Windows.Forms.Button Save;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Assignment 2/Asgn2/Assign2/Assign2/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assign2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            int i = 0;
+
+            //check for a name entered
+            if (((Control)sender).Text.Trim().Length == 0)
+                MessageBox.Show("Please enter a name", "Error");
+
+            //check if user entered a space and reset their input if they did
+            else
+            {
+                while (i++ < ((Control)sender).Text.Length - 1) //Length idea source: https://syntaxdb.com/ref/csharp/string-length
+                {
+                    if (((Control)sender).Text[i].Equals(' '))
+                    {
+                        MessageBox.Show("Name cannot contain a space", "Error");
+                        ((Control)sender).Text = ".";
+                    }
+                }
+            }
+
+        }
+
+        private void Save_Click(object sender, EventArgs e)
+        {
+            Button saveButton = sender as Button;
+            MessageBox.Show("Size was saved");
+            Properties.Settings.Default.UserSize = Size;    //looked at hairons version to figure out what to set here
+            Properties.Settings.Default.Save();
+        }
+
+        private void Location_Click(object sender, EventArgs e)
+        {
+            Button locationButton = sender as Button;
+            MessageBox.Show("Location was saved");
+            Properties.Settings.Default.UserLocation = Form1.ActiveForm.Location;
+            Properties.Settings.Default.Save();
+        }
+
+        private void Reset_Click(object sender, EventArgs e)
+        {
+            Button resetButton = sender as Button;
+            MessageBox.Show("Size + Location was reset");
+            Properties.Settings.Default.Reset();
+        }
+    }
+}
